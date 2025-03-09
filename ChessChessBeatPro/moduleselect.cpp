c364d5249d203f94147819f7bc2cd992d31b6de2
refinedheart <50239539+refinedheart@users.noreply.github.com>
2025-03-09 15:54:02 +0800

    get more contents in Select Room

--- ChessChessBeatPro/moduleselect.cpp
+++ ChessChessBeatPro/moduleselect.cpp
@@ -3,6 +3,8 @@
 #include <QPainter>
 #include <QLabel>
 #include <QFont>
+#include <QPushButton>
+#include <QPalette>
 ModuleSelect::ModuleSelect(QWidget *parent)
     : QWidget{parent}
 {
@@ -15,8 +17,26 @@ ModuleSelect::ModuleSelect(QWidget *parent)
     QLabel *SelectHead = new QLabel(this);
     SelectHead -> setText("选择你的模式！");
     QFont SelectFont("宋体", 50, QFont :: Bold);
+    QPalette HeadP = SelectHead -> palette();
+    HeadP.setColor(QPalette :: WindowText, Qt :: black);
+    SelectHead -> setPalette(HeadP);
     SelectHead -> setFont(SelectFont);
     SelectHead -> move(240, 50);
+    QPushButton *machinebtn = new QPushButton(this);
+    machinebtn -> setText("与机器对战！");
+    QFont machineFont("宋体", 20, QFont :: Bold);
+    machinebtn -> setFont(machineFont);
+    machinebtn -> setFixedSize(200, 80);
+    machinebtn -> move(300, 400);
+    QPalette machineP = machinebtn -> palette();
+    machineP.setColor(QPalette :: ButtonText, Qt :: black);
+    machinebtn->setPalette(machineP);
+    GameRoom *gameroom = new GameRoom;
+    connect(machinebtn, &QPushButton :: clicked, [=]() {
+        this -> close();
+        gameroom -> show();
+    });
+
 }
 
 void ModuleSelect :: paintEvent(QPaintEvent *event) {
