ea16c780d872e1fd92b029001f5abf58bf36abf9
refinedheart <50239539+refinedheart@users.noreply.github.com>
2025-03-09 16:54:30 +0800

    update the game photo

--- ChessChessBeatPro/gameroom.cpp
+++ ChessChessBeatPro/gameroom.cpp
@@ -17,12 +17,19 @@ GameRoom::GameRoom(QWidget *parent)
     connect(backbtn, &QPushButton :: clicked, [=](){
         emit this -> back_to_select();
     });
+    backbtn -> move(800, 700);
+    backbtn -> setFixedSize(150, 50);
+
+    // 页面设置
+    this -> setFixedSize(1000, 800);
 
 }
 
 void GameRoom :: paintEvent(QPaintEvent *event) {
     Q_UNUSED(event);
     QPainter painter(this);
-    QPixmap pix(":/Chess-19.jpeg");
+    QPixmap pix(":/RoomBackGround.jpg");
     painter.drawPixmap(0, 0, this -> width(), this -> height(), pix);
+    pix.load(":/Chess-19.jpeg");
+    painter.drawPixmap(200, 100, 600, 600, pix);
 }
