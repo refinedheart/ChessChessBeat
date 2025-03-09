d0f4be0d5990cb6a9c6946d8f9735f3260af42f9
refinedheart <50239539+refinedheart@users.noreply.github.com>
2025-03-09 10:47:18 +0800

    Link the two classes

--- ChessChessBeatPro/moduleselect.cpp
+++ ChessChessBeatPro/moduleselect.cpp
@@ -1,5 +1,12 @@
 #include "moduleselect.h"
-
+#include "gameroom.h"
 ModuleSelect::ModuleSelect(QWidget *parent)
     : QWidget{parent}
-{}
+{
+    GameRoom *GR = new GameRoom;
+    connect(GR, &GameRoom :: back_to_select, [=](){
+        GR -> close();
+        this -> show();
+    });
+
+}
