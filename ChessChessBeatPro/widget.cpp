6e3d254df636f0f4576feaff09011214cf3fcf88
refinedheart <50239539+refinedheart@users.noreply.github.com>
2025-03-09 09:55:08 +0800

    Draw the start back-ground

--- ChessChessBeatPro/widget.cpp
+++ ChessChessBeatPro/widget.cpp
@@ -1,11 +1,14 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QPainter>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    this -> setFixedSize(800, 500);
+
 }
 
 Widget::~Widget()
@@ -13,7 +16,9 @@ Widget::~Widget()
     delete ui;
 }
 
-void paintEvent(QPaintEvent *event) {
+void Widget :: paintEvent(QPaintEvent *event) {
     Q_UNUSED(event);
-
+    QPainter painter(this);
+    QPixmap pix(":/start-background.webp");
+    painter.drawPixmap(0, 0, this -> width(), this -> height(), pix);
 }
